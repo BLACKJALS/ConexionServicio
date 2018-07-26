45781c2b93f4f580500ffaf07fb443fc8b063932
alejandro.leon <callinferno_666@hotmail.com>
2018-07-26 16:26:38 -0500

    Prueba de conexión a servicio WCF

---
+++ PruebaMicroServicio/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using PruebaMicroServicio.PruebaTutorialServicio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PruebaMicroServicio.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            ServicioPruebaClient client = new ServicioPruebaClient();
+            var correo = client.enviarCorreo("prueba@prueba.co");
+            return View();
+        }
+
+        public ActionResult About() {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact() {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
